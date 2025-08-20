77f274729002ef7e4aaa9dd2adc2a3203021cc38
yeri-1002 <yerikang22@gmail.com>
2025-08-20 17:26:30 +0900

    카테고리 step, end

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 메인 슬라이드
-const swiper = new Swiper('.main-swiper', {
+const swiperMain = new Swiper('.main-swiper', {
   loop: true,
   autoplay: {
     delay: 4000,
@@ -23,3 +23,13 @@ function moveProgressBar(index) {
 }
 
 // 리스트 스와이퍼
+var swiperSub = new Swiper('.swiper', {
+  slidesPerView: 7,  // 한 번에 7개의 슬라이드 표시
+  spaceBetween: 20,  // 슬라이드 간 간격 설정
+  loop: false,  // 끝까지 넘기고 처음으로 돌아가지 않음
+  pagination: {
+    el: '.swiper-pagination',  // 페이지네이션을 표시할 요소
+    clickable: true,  // 페이지네이션 클릭 가능하도록 설정
+    type: 'bullets',  // 점(dot) 형태로 페이지네이션 설정
+  },
+});
