0dc451d84338a9a886078c80111f63ae84dc1198
yeri-1002 <yerikang22@gmail.com>
2025-08-26 21:46:28 +0900

    합치기 step, 베스트 제품 스와이퍼 수정 중

--- script.js
+++ script.js
@@ -84,4 +84,10 @@ new Swiper(".best-swiper", {
   slidesPerView: 5,
   spaceBetween: 20,
   loop: false,
+
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  //   type: 'progressbar',
+  // },
 });
